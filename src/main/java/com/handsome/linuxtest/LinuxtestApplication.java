97a19ca34cdf2b99ad0657984a326163d5fc1bf5
Hand <Hand@2107475922@qq.com>
2023-04-05 18:18:52 +0800

    Initial commit

---
+++ src/main/java/com/handsome/linuxtest/LinuxtestApplication.java
@@ -0,0 +1,13 @@
+package com.handsome.linuxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/handsome/linuxtest/LinuxtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handsome.linuxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
